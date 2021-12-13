0b3acdd5930e135b8a5a340d9df3794d531576e1
hyrious <hyrious@outlook.com>
2021-12-13 10:42:46 +0800

    wip: reset all

--- test/index.ts
+++
@@ -1,6 +0,0 @@
-// node dist/bin.js --define:__ESM__=true test/index.ts
-
-import { importFile, requireFile } from "../src";
-
-console.log("importFile:", await importFile("./test/lib.ts"));
-console.log("requireFile:", await requireFile("./test/lib.ts"));
--- test/throw.ts
+++
@@ -1,5 +0,0 @@
-// run node dist/bin.js test/throw.ts
-// should print the typescript source content
-
-const message: string = "Hello, world!";
-throw new Error(message as never);
